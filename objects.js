958ac39fb8cd772cb12ed9da453ce466f2a8bc16
Navin Subramanian <assasin71003@gmail.com>
2025-04-30 20:47:01 +0530

    Latest updates

---
+++ objects.js
@@ -0,0 +1,133 @@
+const projectData = [
+    {
+        name: "ScanHubGen",
+        description: "Generate Emergency QR to place on accessories.",
+        image: "./Images/projects/scanhubgen.png",
+        category: "website",
+        links: [
+            { text: "Live Demo", url: "https://scanhubgenweb.vercel.app/" },
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/Scanhubgen" }
+        ]
+    },
+    {
+        name: "Geetha Jewellers",
+        description: "Jewellery display website for Geetha Jewellers.",
+        image: "./Images/projects/geetha.png",
+        category: "website",
+        links: [
+            { text: "Live Demo", url: "https://www.geethajewellers.in/" },
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/Jewel" }
+        ]
+    },
+    {
+        name: "Courses Website",
+        description: "Made as a project in the startup.",
+        image: "./Images/projects/shivin.png",
+        category: "website",
+        links: [
+            { text: "Live Demo", url: "https://shivin-tech.vercel.app/" },
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/Shivin_Tech" }
+        ]
+    },
+    {
+        name: "Staff Allocation",
+        description: "Allocate staff to various classrooms. Used by our college.",
+        image: "./Images/projects/staffalloc.png",
+        category: "website",
+        links: [
+            { text: "Live Demo", url: "https://staffalloc.onrender.com/" },
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/StaffallocServer" }
+        ]
+    },
+    {
+        name: "Foundit - UI redesign",
+        description: "This was my first redesign contest.",
+        image: "./Images/projects/foundit.png",
+        category: "ui",
+        links: [
+            { text: "Behance Page", url: "https://www.behance.net/gallery/180377363/Foundit-website-UI-Redesign-contest" }
+        ]
+    },
+    {
+        name: "CookieSpy",
+        description: "Made a all-purpose cookie monitoring extension which does everything a developer requires.",
+        image: "./Images/projects/cookiespy.png",
+        category: "extensions",
+        links: [
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/CookieSpy" }
+        ]
+    },
+    {
+        name: "ClipBoarded",
+        description: "A sleek Chrome extension that lets you save and instantly copy your most-used personal info.",
+        image: "./Images/projects/ClipBoarded.png",
+        category: "extensions",
+        links: [
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/Clipboarded" }
+        ]
+    },
+    {
+        name: "Fluent Path",
+        description: "A website that speciallizes to teach English lessons and vocabulary. For a client.",
+        image: "./Images/projects/fluentpath.png",
+        category: "website",
+        links: [
+            { text: "Live Demo", url: "https://fluent-path-main.vercel.app/" },
+            { text: "GitHub", url: "https://github.com/NavinSubramanian/FluentPath" }
+        ]
+    },
+];
+
+const skillData = [
+    {
+        link: "./Images/logos/java.png"
+    },
+    {
+        link: "./Images/logos/git.png"
+    },
+    {
+        link: "./Images/logos/python2.png"
+    },
+    {
+        link: "./Images/logos/wordpress.png"
+    },
+    {
+        link: "./Images/logos/docker.png"
+    },
+    {
+        link: "./Images/logos/django.png"
+    },
+    {
+        link: "./Images/logos/express.png"
+    },
+    {
+        link: "./Images/logos/nodejs.png"
+    },
+    {
+        link: "./Images/logos/tailwind.png"
+    },
+    {
+        link: "./Images/logos/sass.png"
+    },
+    {
+        link: "./Images/logos/react.png"
+    },
+    {
+        link: "./Images/logos/javascript.png"
+    },
+    {
+        link: "./Images/logos/css.png"
+    },
+    {
+        link: "./Images/logos/html.png"
+    },
+    {
+        link: "./Images/logos/figma.png"
+    },
+    {
+        link: "./Images/logos/aws.png"
+    },
+    {
+        link: "./Images/logos/c++.png"
+    },
+];
